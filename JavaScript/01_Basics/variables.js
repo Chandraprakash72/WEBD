a499e5790c95a17c5cb23d6f433e8557fd4a1a12
Chandra Prakash <hsv7238@gmail.com>
2026-03-31 19:18:14 +0530

    datatype,variablas and console

---
+++ JavaScript/01_Basics/variables.js
@@ -0,0 +1,20 @@
+const accountId =1234553
+let accountEmail ="cpmaurya@gmail.com"
+var accountPassword ="12345"
+accountCity ="Jaipur"
+let accountState;
+
+// accountId=2./
+acccountEmail ="chandu@gmail.com"
+accountPassword="2145525"
+accountCity="Ballia"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+pefer not to use var
+
+becsuse of issue in block scope and functional scope
+
+*/
