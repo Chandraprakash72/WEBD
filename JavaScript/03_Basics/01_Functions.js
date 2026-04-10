20a75bc0f40bcf83bff03f7337424b027555e6f5
Chandra Prakash <hsv7238@gmail.com>
2026-04-10 21:41:11 +0530

    function update

--- JavaScript/03_Basics/01_Functions.js
+++ JavaScript/03_Basics/01_Functions.js
@@ -30,4 +30,38 @@ function loginUserMessage(username){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage())
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+function calculateCartPrice(val1, val2,...num1){ //()...)rest operator used to return all value in array 
+    return num1
+}
+console.log(calculateCartPrice(200,400,500,2000,5004))  
+
+// how to pass object to funcrion
+
+const user ={
+    user: "hitesh",
+    price: 199
+
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user)
+
+ //another way to pass object to functions
+ handleObject({
+    username: "cpmaurya",
+    price : 399
+ })
+
+//   arrray pass to functions
+
+const myNewArray =[200,400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
